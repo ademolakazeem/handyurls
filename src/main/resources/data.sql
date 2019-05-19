8c02ffbadb154910dc90e3a36dd78785de539032
Ademola Kazeem <ademola.kazeem@ucdconnect.ie>
2019-05-19 14:41:59 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users
-(id, name, nnumber)
+(id, name, number)
   SELECT 1 , 'Ademola Kazeem', 'ga67766776'
   WHERE
     NOT EXISTS (
-        SELECT id FROM users WHERE id = 1 and nnumber = 'ga67766776'
+        SELECT id FROM users WHERE id = 1 and number = 'ga67766776'
     );
